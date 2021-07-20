d142ab6216a83d48d9bae7420dc08c311e85ca57
Nick D <nvariad@gmail.com>
2021-07-20 16:19:35 -0400

    Implemented initial methods for Currency Send and Currency Balance commands

--- src/main/java/ca/variantlabs/multicur/Currency.java
+++ src/main/java/ca/variantlabs/multicur/Currency.java
@@ -1,8 +1,22 @@
 package ca.variantlabs.multicur;
 
+import java.util.UUID;
+
 public class Currency {
     public boolean currencyCreate(String[] s){
 
         return true;
+    }
+
+    public static double getCurrency(UUID player){
+        return 2.4;
+    }
+
+    public static void addCurrency(UUID player, double amount){
+        // If there's a failure, throw an exception
+    }
+
+    public static void removeCurrency(UUID player, double amount){
+        // If there's a failure, throw an exception
     }
 }
