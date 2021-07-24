74ea2ad93fccc41b1e4de9600681c6c6ae91cb04
Nick D <nvariad@gmail.com>
2021-07-24 12:44:50 -0400

    Updates to getCurrency for custom currency names

--- src/main/java/ca/variantlabs/multicur/Currency.java
+++ src/main/java/ca/variantlabs/multicur/Currency.java
@@ -1,26 +1,29 @@
 package ca.variantlabs.multicur;
 
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 public class Currency {
-
-    public boolean currencyCreate(String[] s){
-
-        return true;
-    }
-
-    public static String getCurrency(UUID player){
-        String sql = "SELECT bal_currency1 FROM mcur_accounts WHERE uuid=?;";
+    // Method to get the currency balance of a player
+    public static String getCurrency(JavaPlugin plugin, UUID player){
+        Set<String> set_currencies = plugin.getConfig().getConfigurationSection("currency").getKeys(false);
+        List<String> currencies = new ArrayList<>(set_currencies);
+        String currency_name = currencies.get(0);
+        String sql = "SELECT ? FROM mcur_accounts WHERE uuid=?;";
         try {
             PreparedStatement stmt = Multicur.connection.prepareStatement(sql);
-            stmt.setString(1, player.toString());
+            stmt.setString(1, currency_name);
+            stmt.setString(2, player.toString());
             ResultSet results = stmt.executeQuery();
-            return results.getNString("bal_currency1");
+            return results.getNString(currency_name);
         } catch (SQLException e){
             e.printStackTrace();
             return "Error";
