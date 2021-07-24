cac84c3f3316777c5f76eea5d3ed2a381d7bcd7e
Nick D <nvariad@outlook.com>
2021-07-24 16:49:10 -0400

    Implementation of currency add and remove methods

--- src/main/java/ca/variantlabs/multicur/Currency.java
+++ src/main/java/ca/variantlabs/multicur/Currency.java
@@ -1,20 +1,16 @@
 package ca.variantlabs.multicur;
 
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 
 public class Currency {
     // Method to get the currency balance of a player
     public static String getCurrency(JavaPlugin plugin, UUID player){
-        Set<String> set_currencies = plugin.getConfig().getConfigurationSection("currency").getKeys(false);
+        Set<String> set_currencies = Objects.requireNonNull(plugin.getConfig().getConfigurationSection("currency")).getKeys(false);
         List<String> currencies = new ArrayList<>(set_currencies);
         String currency_name = currencies.get(0);
         String sql = "SELECT ? FROM mcur_accounts WHERE uuid=?;";
@@ -30,11 +26,50 @@ public class Currency {
         }
     }
 
-    public static void addCurrency(UUID player, double amount){
+    public static void  addCurrency(JavaPlugin plugin, UUID player, double amount) throws Exception {
         // If there's a failure, throw an exception
+        Set<String> set_currencies = Objects.requireNonNull(plugin.getConfig().getConfigurationSection("currency")).getKeys(false);
+        List<String> currencies = new ArrayList<>(set_currencies);
+        String currency_name = currencies.get(0);
+
+        // Get the current balance to determine what to set the new balance to
+        double current = Double.parseDouble(getCurrency(plugin, player));
+        amount += current;
+
+        String sql = "UPDATE mcur_accounts SET ?=? WHERE uuid=?";
+        try {
+            PreparedStatement stmt = Multicur.connection.prepareStatement(sql);
+            stmt.setString(1, currency_name);
+            stmt.setDouble(2, amount);
+            stmt.setString(3, player.toString());
+            stmt.executeUpdate();
+        } catch(SQLException e){
+            e.printStackTrace();
+            throw new Exception("Error with the Database, see console for error log");
+        }
+
     }
 
-    public static void removeCurrency(UUID player, double amount){
+    public static void removeCurrency(JavaPlugin plugin, UUID player, double amount) throws Exception{
         // If there's a failure, throw an exception
+        Set<String> set_currencies = Objects.requireNonNull(plugin.getConfig().getConfigurationSection("currency")).getKeys(false);
+        List<String> currencies = new ArrayList<>(set_currencies);
+        String currency_name = currencies.get(0);
+
+        // Get the current balance to determine what to set the new balance to
+        double current = Double.parseDouble(getCurrency(plugin, player));
+        amount -= current;
+
+        String sql = "UPDATE mcur_accounts SET ?=? WHERE uuid=?";
+        try {
+            PreparedStatement stmt = Multicur.connection.prepareStatement(sql);
+            stmt.setString(1, currency_name);
+            stmt.setDouble(2, amount);
+            stmt.setString(3, player.toString());
+            stmt.executeUpdate();
+        } catch(SQLException e){
+            e.printStackTrace();
+            throw new Exception("Error with the Database, see console for error log");
+        }
     }
 }
